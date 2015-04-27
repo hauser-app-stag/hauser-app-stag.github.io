222ddb207917c190e50d6c90184eb93f168082fb
Your Name <you@example.com>
2015-04-27 18:43:22 +0300

    ok

--- app-settings.js
+++ app-settings.js
@@ -1,6 +1,6 @@
 (function() {
   window.HauserAppSettings = {
-    apiBaseUrl: "http://hauser-dev.herokuapp.com/api/v1",
+    apiBaseUrl: "http://hauser-dev.herokuapp.com/api/business/v1",
     facebookAppId: "403594233134419",
     facebookAppName: "Hauser",
     firebaseUrl: "https://hauser.firebaseio.com",
