9f588898812d830cd95978e840a45e7fa7ace1c2
Your Name <you@example.com>
2015-04-27 21:15:51 +0300

    ok

--- app-settings.js
+++ app-settings.js
@@ -1,10 +1,10 @@
 (function() {
   window.HauserAppSettings = {
-    apiBaseUrl: "http://hauser-dev.herokuapp.com/api/mobile/v1",
-    facebookAppId: "403594233134419",
-    facebookAppName: "Hauser",
+    apiBaseUrl: "http://hauser-staging.herokuapp.com/api/mobile/v1",
+    facebookAppId: "648750278559403",
+    facebookAppName: "HauserAppStag",
     firebaseUrl: "https://hauser.firebaseio.com",
-    imagesCdnBaseUrl: "http://hauser-dev.herokuapp.com/images",
+    imagesCdnBaseUrl: "http://hauser-staging.herokuapp.com/images",
     
     // apiBaseUrl: "http://localhost:3000/api/mobile/v1",
     // facebookAppId: "1555600084706545",
